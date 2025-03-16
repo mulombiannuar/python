8755f3d34cfabe84a1c0adb7f0e157f670d0bc7c
Annuar Mulombi <mulombiannuar@gmail.com>
2025-03-16 16:44:01 +0300

    improvements

--- Predicting House Price/app.py
+++ Predicting House Price/app.py
@@ -11,7 +11,7 @@ prediction_model = joblib.load('house_price_prediction_model.pkl')
 st.title('Linear Regression Prediction App')
 st.write('Enter feature value to get prediction')
 
-feature_value = st.number_input('Enter feature value', min_value=500, max_value=50000, value=1200)
+feature_value = st.number_input(label='Enter feature value', min_value=500, max_value=50000, value=1200)
 if st.button('Predict House Price'):
     prediction = prediction_model.predict(pd.DataFrame([[feature_value]], columns=['HouseSize']))
     #st.write(f'Prediction value is : {prediction[0]:.2f}')
