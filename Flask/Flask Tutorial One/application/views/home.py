c8de225cf24065dce17644cde5ccbd3b57509bcd
Anuary Mulombi <mulombiannuar@gmail.com>
2025-04-29 21:52:39 +0300

    further modification

--- Flask/Flask Tutorial One/application/views/home.py
+++ Flask/Flask Tutorial One/application/views/home.py
@@ -1,13 +1,14 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
-from application.models.post import Post
+from application.forms.user_form import RegistrationForm
 
 home = Blueprint('home', __name__)
 
 
 @home.route('/', methods=['GET'])
 def home_page():
-    return render_template('auth/login.html')
+    form = RegistrationForm()
+    return render_template('auth/login.html', form=form)
 
 
 @home.route('/dashboard', methods=['GET'])
