a5f5255b781d1ef7ed387b73b470e6d71de6d8f2
Anuary Mulombi <mulombiannuar@gmail.com>
2025-05-01 21:22:20 +0300

    updated directories

--- Flask/Flask Tutorial One/application/views/user.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
-from application import db   #means from __init__.py import db
-from flask_login import login_required, logout_user, current_user
-
-user = Blueprint('user', __name__)
